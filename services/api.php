e5d5795fe65bb977fee9e97694ccf361c5e17769
Bogdan-Ioan BRUDIU <bogdanbrudiu@gmail.com>
2015-11-19 11:28:19 +0000

    Openshift connection issues

--- services/api.php
+++ services/api.php
@@ -1,5 +1,5 @@
 <?php
-	ini_set('display_errors','On'); ini_set('error_reporting','E_ALL | E_STRICT'); error_reporting(E_ALL);
+	//ini_set('display_errors','On'); ini_set('error_reporting','E_ALL | E_STRICT'); error_reporting(E_ALL);
  	
  	require_once("Rest.inc.php");
 
